757e6ad56821ada59d3041486bf31f935fbb358a
Brijesh-mca <ibrijesh.mca@gmail.com>
2025-02-13 20:32:31 +0530

    adding all files

---
+++ script.js
@@ -0,0 +1,37 @@
+document.getElementById("booking-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Thank you! Your trip request has been submitted.");
+});
+
+
+//
+// loader
+
+let user  = document.getElementById("userText");
+
+// let email = localStorage.getItem(email,email)
+// user.textContent = email
+window.onload = function() {
+    setTimeout(() => {
+        document.getElementById("loader").style.display = "none";
+        // document.getElementById("content").style.display = "block";
+    }, 2200); //
+};
+
+//gsap animation
+
+gsap.from("#boxx",{
+    y:-100,
+    scale:0,
+    duration:0.3,
+    delay:2,
+    stagger:0.3
+})
+
+gsap.from("#boxxx",{
+    y:-100,
+    scale:0,
+    duration:0.5,
+    delay:2,
+    stagger:0.3
+})
